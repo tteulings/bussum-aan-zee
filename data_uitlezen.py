03d10a103c449210c9d3eadcfff9e2a61a92c464
Joris Smeets <jepsmeets@hotmail.com>
2021-01-12 15:31:30 +0100

    data per maand

--- data_uitlezen.py
+++ data_uitlezen.py
@@ -4,3 +4,55 @@ data = pd.read_csv("datasealvl.txt", header=None, sep='\s+')
 data.columns = ["altimeter", "cycle", "year", "observations", "weighted_observations", "GMSLNA", "std_GMSLNA", "smoothed_GMSLNA", "GMSLA", "std_GMSLA", "smoothed_GMSLA", "smoothed_GMSLA_(semi)-an_rm"]
 data.to_csv("sealevel_data.csv", index=False)
 
+print(data["year"])
+januari = []
+februari = []
+maart = []
+april = []
+mei = [] 
+juni = [] 
+juli = [] 
+augustus = [] 
+september = [] 
+oktober = [] 
+november = []
+december = []
+i = 0
+j = 0
+temp = 0
+for a in data["GMSLNA"]:
+    if i%3 == 0:
+        if j == 0 and i != 0:
+            januari.append(temp/3)
+        elif j == 1:
+            februari.append(temp/3)
+        elif j == 2:
+            maart.append(temp/3)
+        elif j == 3:
+            april.append(temp/3)
+        elif j == 4:
+            mei.append(temp/3)
+        elif j == 5:
+            juni.append(temp/3)
+        elif j == 6:
+            juli.append(temp/3)
+        elif j == 7:
+            augustus.append(temp/3)
+        elif j == 8:
+            september.append(temp/3)
+        elif j == 9:
+            oktober.append(temp/3)
+        elif j == 10:
+            november.append(temp/3)
+        else:
+            december.append(temp/3)
+            j = -1 
+        
+        j+=1
+        temp = 0
+    temp =+a
+    i +=1
+
+print(januari)
+    
+    
